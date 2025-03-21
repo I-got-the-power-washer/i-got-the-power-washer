7ef108e7d49a9ea569134ca3507e94268c66dcc2
ahsan-bpo <igotpowerwash.dev@gmail.com>
2025-03-21 20:51:54 +0500

    forms

--- src/app/page.js
+++ src/app/page.js
@@ -17,11 +17,11 @@ export default function Home() {
      <ServicesSection/>
      <CommercialServicesSection/>
      <ClientLogos/>
-   
-     <RequestEstimateForm/>
+{/*    
+     <RequestEstimateForm/> */}
      <HomeExterior/>
    <Review/>
-     {/* <FAQ/> */}
+     <FAQ/>
      
     
     </div>
