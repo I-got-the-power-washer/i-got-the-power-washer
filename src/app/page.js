356827c30ab1b11a4863484b3aa91b0b15efa1b7
ahsan-bpo <igotpowerwash.dev@gmail.com>
2025-03-25 00:45:20 +0500

    loading

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useState } from "react";
 import ServicesSection from "./components/Card/Card";
 import ClientLogos from "./components/Logos/logos";
 import FAQ from "./components/Faq/faq";
@@ -5,27 +7,38 @@ import RequestEstimateForm from "./components/homeForm/RequestEstimateForm";
 import HomeExterior from "./components/homeExterior/homeExterior";
 import CommercialServicesSection from "./components/Commercial/Commercial";
 import Review from "./components/Review/Review";
-import HeroSection from "./components/Hero/hero";
+
 import ResponsiveImage from "./components/Image/image";
+import Loader from "./components/Loading/Loading";
+// import Loader from "./components/Loader"; // 👈 import loader component
 
 export default function Home() {
-  return (
-    <div className="">
-      {/* <HeroSection /> */}
-      <ResponsiveImage
-  src="/images/Web-1-1536x1152 (1).webp"
-  alt="Professional pressure washing services in Cincinnati"
-  className="aspect-[4/3] w-full" // 4:3 ratio matching your image dimensions
-  priority={true}
-/>
-      <ServicesSection />
-      <CommercialServicesSection />
-      <ClientLogos />
+  const [loading, setLoading] = useState(true);
 
-      <RequestEstimateForm />
-      <HomeExterior />
-      <Review />
-      <FAQ />
-    </div>
+
+  
+
+  return (
+    <>
+      {loading && <Loader onFinish={() => setLoading(false)} />}
+      {!loading && (
+        <div className="">
+          {/* <HeroSection /> */}
+          <ResponsiveImage
+            src="/images/Web-1-1536x1152 (1).webp"
+            alt="Professional pressure washing services in Cincinnati"
+            className="aspect-[4/3] w-full"
+            priority={true}
+          />
+          <ServicesSection />
+          <CommercialServicesSection />
+          <ClientLogos />
+          <RequestEstimateForm />
+          <HomeExterior />
+          <Review />
+          <FAQ />
+        </div>
+      )}
+    </>
   );
 }
